86f55559940f4b6b7a02c8ae17a63a17a78c5598
Toni Pesola <pesola.toni@gmail.com>
2020-08-08 13:17:31 +0300

    Config, Gamepad suport

---
+++ config/kb_config.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define KB_CONFIG_MAX_GAMEPADS       8
+#define KB_CONFIG_MAX_BATCHES        1024
+#define KB_CONFIG_MAX_ENCODERS       1024
+#define KB_CONFIG_MAX_FRAMEBUFFERS   1024
+#define KB_CONFIG_MAX_INDEX_BUFFERS  1024
+#define KB_CONFIG_MAX_MATERIALS      1024
+#define KB_CONFIG_MAX_MESHES         1024
+#define KB_CONFIG_MAX_PROGRAMS       1024
+#define KB_CONFIG_MAX_SHADERS        1024
+#define KB_CONFIG_MAX_SURFACES       1
+#define KB_CONFIG_MAX_TEXTURES       1024
+#define KB_CONFIG_MAX_UNIFORMS       1024
+#define KB_CONFIG_MAX_UNIFORM_BLOCKS 1024
+#define KB_CONFIG_MAX_VERTEX_BUFFERS 1024
+#define KB_CONFIG_MAX_VIEWS          1024
+#define KB_CONFIG_MAX_MUSICS         1024
+#define KB_CONFIG_MAX_SOUNDS         1024
+#define KB_CONFIG_FILE_ROOT_SIZE     512
+
+#ifdef __cplusplus
+}
+#endif
