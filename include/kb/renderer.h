42d2ba379dac3f25d73f6d70d390757367874711
Toni Pesola <pesola.toni@gmail.com>
2020-09-07 00:17:11 +0300

    Some cleaning. Multiple draw submits works
    Former-commit-id: 400062d3dbe46d532d9044ba1c939d6dcbc11422

--- include/kb/renderer.h
+++ include/kb/renderer.h
@@ -121,10 +121,10 @@ KB_RESOURCE_CORE_FUNC_DECLS   (vertex_buffer  , VertexBufferHandle  , VertexBuff
 KB_API CommandBufferHandle  kb_command_buffer_begin             ();
 KB_API void                 kb_command_buffer_end               (CommandBufferHandle command_buffer);
 
-KB_API void                 kb_command_buffer_set_vertex_buffer (CommandBufferHandle command_buffer, VertexBufferHandle handle);
-KB_API void                 kb_command_buffer_set_index_buffer  (CommandBufferHandle command_buffer, IndexBufferHandle handle);
+KB_API void                 kb_command_buffer_bind_vertex_buffer (CommandBufferHandle command_buffer, VertexBufferHandle handle);
+KB_API void                 kb_command_buffer_bind_index_buffer  (CommandBufferHandle command_buffer, IndexBufferHandle handle);
 KB_API void                 kb_command_buffer_set_mesh          (CommandBufferHandle command_buffer, MeshHandle handle);
-KB_API void                 kb_command_buffer_set_program       (CommandBufferHandle command_buffer, ProgramHandle handle);
+KB_API void                 kb_command_buffer_bind_program       (CommandBufferHandle command_buffer, ProgramHandle handle);
 KB_API void                 kb_command_buffer_set_viewport      (CommandBufferHandle command_buffer, Float2 size, Float2 depth_range);
 KB_API void                 kb_command_buffer_set_scissors      (CommandBufferHandle command_buffer, Int2 extent, Int2 offset);
 KB_API void                 kb_command_buffer_set_push_constants(CommandBufferHandle command_buffer, void* data, size_t size);
@@ -137,7 +137,7 @@ KB_API void                 kb_graphics_init                    (const GraphicsI
 KB_API void                 kb_graphics_deinit                  ();
 KB_API void                 kb_graphics_frame                   ();
 KB_API void                 kb_graphics_frame_tmp_render        ();
-
+KB_API Int2                 kb_graphics_get_extent              ();
 KB_API void                 kb_graphics_wait_device_idle        ();
 
 #ifdef __cplusplus
