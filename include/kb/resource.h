c250cfabc77a34099b5c0aab8949580b9bf220a1
Toni Pesola <pesola.toni@gmail.com>
2020-09-26 20:31:55 +0300

    Purge before exit
    Former-commit-id: 82c6363788789f0bbb6c67075cd3f9362bf94be8

--- include/kb/resource.h
+++ include/kb/resource.h
@@ -28,6 +28,7 @@ extern "C" {
 #include <kb/freelist.h>
 #include <kb/table.h>
 #include <kb/crt.h>
+#include <kb/log.h>
 
 template <typename Handle, typename CreateInfo, typename Ref, uint16_t Capacity>
 struct ResourceData {
@@ -42,6 +43,13 @@ struct ResourceData {
     kb_freelist_destroy  (&freelist);
   }
   
+  void purge() {
+    uint32_t* dense = kb_freelist_get_dense(&freelist);
+    for (uint32_t i = 0; i < freelist.count; ++i) {
+      kb_log_debug("Purge: ({}) {}", i, dense[i]);
+    }
+  }
+
   Ref       refs      [Capacity];
   uint16_t  refcount  [Capacity];
   Freelist  freelist;
@@ -78,11 +86,18 @@ struct ResourceData {
   }                                                                                               \
   uint32_t kb_##t_name##_count() {                                                                \
     return kb_freelist_count(&(t_name##_data.freelist));                                          \
+  }                                                                                               \
+  void kb_##t_name##_purge() {                                                                    \
+    uint32_t* dense = kb_freelist_get_dense(&t_name##_data.freelist);                             \
+    uint32_t count = kb_##t_name##_count();                                                       \
+    for (uint32_t i = 0; i < count; ++i) {                                                        \
+      kb_##t_name##_destroy({dense[0]});                                                          \
+    }                                                                                             \
   }   
 
 #define KB_RESOURCE_DATA_HASHED_DEF(t_name, handle_t)                                             \
-  void kb_##t_name##_set_hash(handle_t handle, Hash _hash) {                                       \
-    kb_table_insert(&(t_name##_data.table), _hash, handle.idx);                                    \
+  void kb_##t_name##_set_hash(handle_t handle, Hash _hash) {                                      \
+    kb_table_insert(&(t_name##_data.table), _hash, handle.idx);                                   \
   }                                                                                               \
   void kb_##t_name##_set_name(handle_t handle, const char* name) {                                \
     kb_##t_name##_set_hash(handle, kb_hash_string(name));                                         \
